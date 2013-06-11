373f4506492f84ef88e6c4038c547a862a422946
cromine87 <cromine87@gmail.com>
2013-06-11 18:15:39 -0500

    Update config.php

--- WebsiteCode/config.php
+++ WebsiteCode/config.php
@@ -1,5 +1,5 @@
 <?php 
 $con = mysql_connect("localhost","gamecastlacrosse","uvuINFO4430!") or die(mysql_error());
-mysql_select_db("db_name") or die(mysql_error());
+mysql_select_db("gamecastlacrosse") or die(mysql_error());
 
 ?>
