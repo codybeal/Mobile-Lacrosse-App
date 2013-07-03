25f8eeab27f4874d82b6971dd6a7e57bfeeeb8ca
Callen Barton <cal5barton@yahoo.com>
2013-07-02 18:20:07 -0600

    Player View

--- DB Scripts/Create Views/Views.sql
+++ DB Scripts/Create Views/Views.sql
@@ -31,3 +31,21 @@ FROM game LEFT JOIN game_current_score
 	ON home_team.team_id = home_totals.team_id LEFT JOIN team_totals AS vis_totals
 	ON vis_team.team_id = vis_totals.team_id
 WHERE game.game_date < CURDATE()
+
+;
+
+CREATE VIEW player_stats
+AS
+SELECT player.player_id, team.team_id, game.game_id, player.name, player.uniform, 
+	shots.goals, shots.assists, shots.shots, shots.shots_on_goal, shots.free_position AS free_position_shots, shots.shootout AS shootout_shots,
+	misc.face_won, misc.face_lost, misc.ground_balls, misc.draw_controls, misc.turnovers, misc.caused_turnovers, misc.minutes_played, misc.seconds_played,
+	goal_type.game_winning, goal_type.power_play, goal_type.short_handed, goal_type.unassisted, goal_type.first_goals, goal_type.over_time, goal_type.empty_net, goal_type.game_tying, goal_type.free_position AS free_position_goals, goal_type.shootout AS shootout_goals,
+	goalie.win, goalie.loss, goalie.tie, goalie.power_play_goals_allowed, goalie.short_handed_goals_allowed, goalie.empty_net_goals_allowed, goalie.shootout_goals_allowed, goalie.saves
+FROM player LEFT JOIN team
+	ON player.team_id = team.team_id LEFT JOIN player_shots AS shots
+	ON player.player_id = shots.player_id LEFT JOIN player_penalties AS penalties
+	ON player.player_id = penalties.player_id LEFT JOIN player_misc AS misc
+	ON player.player_id = misc.player_id LEFT JOIN player_goalie AS goalie
+	ON player.player_id = goalie.player_id LEFT JOIN player_goal_type AS goal_type
+	ON player.player_id = goal_type.player_id LEFT JOIN game
+	ON shots.game_id
